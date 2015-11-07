56807b1437df3cca19f75e4cb733a0c9b0481cf7
drewcovi <drew.covi@me.com>
2015-11-07 15:20:12 -0600

    udpates

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(".special").click(function(){
-alert("Ouch! My face! Please don't be a nerd and slap my face.");
+alert("Ouch! My face! Please don't be a nerd and slap my face. This website was made for Sophia Clements.");
 $("body").css("background","orange")
 
 });
